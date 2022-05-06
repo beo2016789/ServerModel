921a83bfe28d5bb6f34676deaf6499078076373f
dat <you@example.com>
2022-05-06 16:03:54 +0700

    Initial commit

---
+++ server_model/run_model.py
@@ -0,0 +1,39 @@
+import os
+import librosa
+import numpy as np
+import math
+import pickle
+
+labels = ["BK", "turnOff", "turnOn"]
+commands = ["hey BK", "turn off", "turn on"]
+
+def removeSilence(audio):
+    clip = librosa.effects.trim(audio, top_db = 10)
+    return clip[0]
+
+def get_mfcc(file_path):
+    y, sr = librosa.load(file_path)
+    y = removeSilence(y)
+    hop_length = math.floor(sr*0.01)
+    win_length = math.floor(sr*0.025)
+    mfcc = librosa.feature.mfcc(
+        y, sr, n_mfcc=12, n_fft = 1024, hop_length=hop_length, win_length=win_length,
+    )
+    mfcc = mfcc - np.mean(mfcc, axis=1).reshape((-1, 1))
+    delta1 = librosa.feature.delta(mfcc, order=1)
+    delta2 = librosa.feature.delta(mfcc, order=2)
+
+    X = np.concatenate([mfcc, delta1, delta2], axis=0)
+    return X.T
+
+model = {}
+for key in labels:
+    name = f"server_model/models/models_train_model_{key}.pkl"
+    with open(name, 'rb') as file:
+        model[key] = pickle.load(file)
+
+def getPredict(filePath):
+    record_mfcc = get_mfcc(filePath)
+    scores = [model[label].score(record_mfcc) for label in labels]
+    index = np.argmax(scores)
+    return commands[index]
---
+++ server_model/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import base64
+import json
+from .run_model import getPredict
+# Create your views here.
+@api_view(['POST'])
+def predict(request):
+    encode_text = json.loads(request.body)['fileCode']
+    decode_text = base64.b64decode(encode_text)
+    file_wav = open('temp.wav', 'wb')
+    file_wav.write(decode_text)
+    return Response({'result': getPredict('temp.wav')})
